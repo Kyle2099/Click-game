2bc3c0ac9a25aed126d6a9d657f4d94cf5fae208
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2018-06-19 21:37:17 -0500

    Working UI page

--- clicky/src/components/FriendCard/FriendCard.js
+++ clicky/src/components/FriendCard/FriendCard.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "./FriendCard.css";
 
+// const FriendCard = props => (
+//   <div className="card">
+//     <div className="img-container">
+//       <img alt={props.name} src={props.image} />
+//     </div>
+//     <div className="content">
+//       <ul>
+//         <li>
+//         </li>
+//         <li>
+
+//         </li>
+//         <li>
+         
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// );
 const FriendCard = props => (
-  <div className="card">
+  <div 
+    className="card" 
+    value={props.id} 
+    onClick={() => props.handleClick(props.id)}
+  >
     <div className="img-container">
       <img alt={props.name} src={props.image} />
-    </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>{props.image}</strong> {props.image}
-        </li>
-        <li>
-
-        </li>
-        <li>
-         
-        </li>
-      </ul>
     </div>
   </div>
 );
